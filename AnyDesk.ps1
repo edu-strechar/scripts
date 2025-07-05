07025927edf96dac6fa517d07aa280e3f2508fdb
edu-strechar <edu_strechar@hotmail.com>
2025-07-04 21:23:03 -0300

    Create AnyDesk.ps1

---
+++ AnyDesk.ps1
@@ -0,0 +1,4 @@
+$url = "https://download.anydesk.com/AnyDesk.exe"
+    $installerPath = "$env:USERPROFILE\Downloads\AnyDesk.exe"
+    Invoke-WebRequest -Uri $url -OutFile $installerPath
+    & $installerPath --silent --install-dir="C:\Program Files\AnyDesk" --no-auto-update
