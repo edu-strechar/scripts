d29d88ef9600159fabfac9163e205f058f1997e0
edu-strechar <edu_strechar@hotmail.com>
2025-07-04 23:04:56 -0300

    Update AnyDesk.bat

--- AnyDesk.bat
+++ AnyDesk.bat
@@ -1,4 +1,6 @@
 @echo off 
+
 AnyDesk.exe --install "C:\Install\AnyDesk\Here" --start-with-win --silent --create-shortcuts --create-desktop-icon 
+
 echo licence_keyABC | "C:\Install\AnyDesk\Here\AnyDesk.exe" --register-licence 
 echo password123 | "C:\Install\AnyDesk\Here\AnyDesk.exe" --set-password
