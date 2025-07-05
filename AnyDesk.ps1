504ef28c6a57f013742acd0ae85e15cf778012b2
edu-strechar <edu_strechar@hotmail.com>
2025-07-05 00:03:57 -0300

    Update AnyDesk.ps1

--- AnyDesk.ps1
+++ AnyDesk.ps1
@@ -1,3 +1,12 @@
-      $InstallerPath = "C:\path\to\AnyDesk.exe" # Replace with the actual path
-      Invoke-WebRequest -Uri "https://download.anydesk.com/AnyDesk.exe" -OutFile $InstallerPath
-      Start-Process -FilePath $InstallerPath -ArgumentList "/S" -Wait
+$AnyDeskURL = "https://download.anydesk.com/AnyDesk.exe"
+$InstallPath = "$env:TEMP\AnyDesk.exe"
+$Arguments = "--silent --install --start-with-win --create-desktop-link"
+
+# Baixa o instalador
+Invoke-WebRequest -Uri $AnyDeskURL -OutFile $InstallPath
+
+# Executa o instalador
+Start-Process -FilePath $InstallPath -ArgumentList $Arguments -Wait -NoNewWindow
+
+# Remove o instalador
+Remove-Item $InstallPath
